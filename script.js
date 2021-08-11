abcf89b7d30672a6c08c7724957e4e9348ba63d7
pa-ul-dev <78130276+pa-ul-dev@users.noreply.github.com>
2021-08-11 17:02:43 -0500

    pds
    pds

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var myHeaders = new Headers();
+myHeaders.append("X-CloudBoxx-ApiKey", "5k0hxNLpkEqhPfql4v8MzNq86q+Xboy4NUalg7lJlerh0iFckIokq9PuZYuBu1Eb");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://api.cloudboxx.invers.com/api/devices/", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
